7ede4470145af5953a2782c5cc5788da35e0ceef
Shivank Rana <shivankraana@gmail.com>
2026-05-03 23:12:39 +0530

    fix alias imports

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,11 @@
-import Header from '@/components/Header'
-import SystemGraphHero from '@/components/SystemGraphHero'
-import EnginStack from '@/components/EnginStack'
-import UseCases from '@/components/UseCases'
-import TrustLayer from '@/components/TrustLayer'
-import SystemInspectorPanel from '@/components/SystemInspectorPanel'
-import PlatformSection from '@/components/PlatformSection'
-import Footer from '@/components/Footer'
+import Header from '../components/Header'
+import SystemGraphHero from '../components/SystemGraphHero'
+import EnginStack from '../components/EnginStack'
+import UseCases from '../components/UseCases'
+import TrustLayer from '../components/TrustLayer'
+import SystemInspectorPanel from '../components/SystemInspectorPanel'
+import PlatformSection from '../components/PlatformSection'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
